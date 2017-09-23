19ac32a63c462c6e51e64f3279ef4eacd7a63af5
Ryosuke Kakehashi <kakepeer@gmail.com>
2017-09-24 00:28:47 +0900

    head title を設定

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,6 @@
 class NotesController < ApplicationController
   before_action :set_note, only: :show
+  before_action :set_title
 
   def index
     @notes = Note.all
@@ -12,5 +13,14 @@ class NotesController < ApplicationController
 
   def set_note
     @note = Note.find(params[:id])
+  end
+
+  def set_title
+    case action_name
+    when "index"
+      @title = "Notes｜KAKETAN.COM"
+    when "show"
+      @title = "#{@note&.title}｜KAKETAN.COM"
+    end
   end
 end
