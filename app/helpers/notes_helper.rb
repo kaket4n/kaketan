ab6eb754705fee0710445b5b80acff6a6254b096
Ryosuke Kakehashi <kakepeer@gmail.com>
2017-09-30 21:17:16 +0900

    Added restrictable

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -6,4 +6,20 @@ module NotesHelper
   def hatena_share_button(url:nil)
     "<a href='#{url}' class='hatena-bookmark-button' data-hatena-bookmark-layout='basic-label-counter' data-hatena-bookmark-lang='ja' title='このエントリーをはてなブックマークに追加'><img src='https://b.st-hatena.com/images/entry-button/button-only@2x.png' alt='このエントリーをはてなブックマークに追加' width='20' height='20' style='border: none;' /></a><script type='text/javascript' src='https://b.st-hatena.com/js/bookmark_button.js' charset='utf-8' async='async'></script>".html_safe
   end
+
+  def restrictable(note)
+    if user_signed_in?
+      note
+    else
+      note.gsub(/<secret>(\r|\n)*.*(\r|\n)*<\/secret>/, invitation_text)
+    end
+  end
+
+  private
+
+  def invitation_text
+    <<-EOS
+      <p class="h4">ここから先はチャンネル登録者のみ閲覧可能です。</p><div class="text-center"><a href="/users/sign_up"><span class="button">無料でチャンネル登録する</span></a></div>
+    EOS
+  end
 end
