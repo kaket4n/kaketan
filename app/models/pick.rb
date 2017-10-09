4a1ff223b4a52a1e10cdcd6a0cf9ca382d5faf45
Ryosuke Kakehashi <kakepeer@gmail.com>
2017-10-09 12:18:18 +0900

    description と img_url のバリデーションを追加

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -1,5 +1,7 @@
 class Pick < ApplicationRecord
-  before_create :assign_attr_with_scraping
+  before_validation :assign_attr_with_scraping
+  validate :description_must_be_present
+  validate :img_url_must_be_present
 
   private
 
@@ -9,5 +11,18 @@ class Pick < ApplicationRecord
     self.title = page.title
     self.description = page.at('meta[@property="og:description"]')["content"]
     self.img_url = page.at('meta[@property="og:image"]')["content"]
+  rescue => e
+  end
+
+  def description_must_be_present
+    if description.blank?
+      errors.add(:description, "description の取得に失敗しました。")
+    end
+  end
+
+  def img_url_must_be_present
+    if img_url.blank?
+      errors.add(:img_url, "img_url の取得に失敗しました。")
+    end
   end
 end
