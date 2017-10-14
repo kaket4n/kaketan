07b0db06438755d4191aa3b95f36c31c6e7e5741
Ryosuke Kakehashi <kakepeer@gmail.com>
2017-10-14 17:39:57 +0900

    Added twitter and config

--- app/controllers/admin/picks_controller.rb
+++ app/controllers/admin/picks_controller.rb
@@ -15,6 +15,16 @@ class Admin::PicksController < AdminController
     @pick = Pick.new(pick_params)
 
     if @pick.save
+      client = ::Twitter::REST::Client.new do |config|
+        config.consumer_key        = Settings.twitter.key
+        config.consumer_secret     = Settings.twitter.secret
+        config.access_token        = Settings.twitter.token
+        config.access_token_secret = Settings.twitter.token_secret
+      end
+
+      tweet = "#{@pick.memo} / #{@pick.title}｜KAKETAN.COM #{pick_url(@pick)} #KAKETANCOM"
+      client.update(tweet)
+
       redirect_to admin_pick_path(@pick)
     else
       render :new
