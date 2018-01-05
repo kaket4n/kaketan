0440f667c0cfd068c667ff9df9ef802df287193b
Ryosuke Kakehashi <kakepeer@gmail.com>
2018-01-05 18:20:39 +0900

    不要な include を削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,11 +37,9 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
-  # config.include Devise::Test::ControllerHelpers, type: :helper
   config.include FactoryBot::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers, type: :helper
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
-  config.include ControllerMacros, type: :helper
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
