76dcdac0d3c86bcb9905f74dbeead2962a332643
Ryosuke Kakehashi <kakepeer@gmail.com>
2018-01-30 00:28:07 +0900

    Add ImageMagickPath and CommandPath

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,4 @@
 Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
 Paperclip::Attachment.default_options[:path] = '/:style/:filename'
+Paperclip.options[:image_magick_path] = "/usr/local/bin/magick"
+Paperclip.options[:command_path] = "/usr/local/bin/"
