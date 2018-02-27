14a63e01c8c5eb444e09d034d86bdca7e5c0c268
Ryosuke Kakehashi <kakepeer@gmail.com>
2018-02-27 12:37:55 +0900

    コメントが書かれたらメールが届くようにした

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,9 +4,15 @@ class Comment < ApplicationRecord
 
   belongs_to :diary
 
+  after_create_commit :notify_admin
+
   private
 
   def login_required
     errors.add(:user_id, "ログインが必要です") if user_id.nil?
+  end
+
+  def notify_admin
+    NotificationMailer.notify_admin(self).deliver_later
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,14 +30,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV['MAIL_USER_NAME'],
-    :password => ENV['MAIL_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
   config.action_mailer.perform_caching = false
 
