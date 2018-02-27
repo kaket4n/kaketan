76aa17141d44341fa93b99449476d994cfff14d1
Ryosuke Kakehashi <kakepeer@gmail.com>
2018-02-27 14:03:41 +0900

    コメントした日記にコメントが書かれたらメールが送られるようにした

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -7,4 +7,18 @@ class NotificationMailer < ApplicationMailer
       mail(to: "kaketancom@gmail.com", subject: "日記にコメントが書かれました。")
     end
   end
+
+  def notify_users(current_comment)
+    @comment = current_comment
+    @diary = current_comment&.diary
+
+    return if @diary.nil?
+
+    target_user_ids = @diary.comments.includes(:user).map(&:user).pluck(:id).uniq
+    target_user_ids.delete(current_comment.user.id)
+
+    User.where(id: target_user_ids).each do |user|
+      mail(to: user.email, subject: "あなたがコメントした日記にコメントが書かれました。")
+    end
+  end
 end
